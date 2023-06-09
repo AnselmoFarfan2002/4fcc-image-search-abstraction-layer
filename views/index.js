4e259e1732fc64b477569c6ab2d35c4fbacd3058
AnselmoFarfan2002 <anselmofarfan2002@gmail.com>
2023-06-09 13:02:29 -0500

    dark screen

--- views/index.js
+++ views/index.js
@@ -1,9 +1,9 @@
 var queryLink = document.getElementById('queryLink')
-const HOST = document.getElementById('queryLink').innerText + '/'
+const HOST = document.getElementById('queryLink').innerText
 
 const queryInput = document.getElementById('query')
 queryInput.onkeyup = function(target){
-    queryLink.innerHTML = HOST + encodeURIComponent(queryInput.value)
+    queryLink.innerHTML = HOST + 'query/' + encodeURIComponent(queryInput.value)
     eventPageInput()
 }
 
@@ -11,6 +11,8 @@ const pageInput = document.getElementById('page')
 function eventPageInput(target){ 
     if(pageInput.value.length)
         queryLink.innerHTML = HOST + queryInput.value + `?page=` + pageInput.value 
+    
+    queryLink.href = queryLink.innerHTML
 }
 pageInput.onchange = eventPageInput
 pageInput.onkeyup = eventPageInput
